f4f419e0999278c45e7797a75ed0c3973f81278c
Aryamaan Pandey <aryamaanpandey@L-TMVR6D67G9.local>
2023-03-01 14:14:10 +0530

    delete folder

--- BMarketsUlt/app/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from . models import CustomUser
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        
-        model = CustomUser
-        fields = ('id', 'username', 'password', 'email', 'first_name', 'middle_name', 'last_name','pan_number')

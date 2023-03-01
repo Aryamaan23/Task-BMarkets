3c79c1df0dc38103f324adcfb2ff25cd85a7891c
Aryamaan Pandey <aryamaanpandey@L-TMVR6D67G9.local>
2023-03-01 19:49:20 +0530

    Added Swagger UI

--- BmarketNew/BmarketNew/settings.py
+++ BmarketNew/BmarketNew/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'app',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [

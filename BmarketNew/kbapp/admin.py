0709f8af1aa38382d8e3a1e5b87cd6d55dfce603
Aryamaan Pandey <aryamaanpandey@L-TMVR6D67G9.local>
2023-03-08 16:53:54 +0530

    For adding all the fields in the list display of django admin

--- BmarketNew/kbapp/admin.py
+++ BmarketNew/kbapp/admin.py
@@ -11,10 +11,30 @@ from django.core.exceptions import ValidationError
 class AmcAdmin(admin.ModelAdmin):
 
     model = Amc
-    list_display = ('name','created','modified','is_active','modified_by','created_by','name','amfi_nav_download_dropdown_code',
-                   'amc_assets_under_management','description','is_being_sold','f_amc_code','amc_logo','amc_website_url','scheme_information_document_url','nominee_url'
-                   )
+    list_display = [field.name for field in Amc._meta.fields]
 
 admin.site.register(Amc, AmcAdmin)
 
 
+
+"""
+ expense_ratio_url = models.CharField(max_length=200, null=True, blank=True)
+    expense_ratio_url_remarks = models.CharField(max_length=200, null=True, blank=True)
+    last_nav_pull_date_from_amfi = models.DateField(null=True, blank=True)
+    rta_amc_code = models.CharField(max_length=64, null=True, blank=True)
+    is_isip_available = models.BooleanField(default=True)
+    cio = models.CharField(max_length=150, null=True, blank=True)
+    ceo = models.CharField(max_length=150, null=True, blank=True)
+    management_trustee = models.CharField(max_length=200, null=True, blank=True)
+    start_date = models.DateField(null=True, blank=True)
+    phone = models.CharField(max_length=50, null=True, blank=True)
+    email = models.CharField(max_length=100, null=True, blank=True)
+    owner_type = models.CharField(max_length=50, null=True, blank=True)
+    address1 = models.CharField(max_length=200, null=True, blank=True)
+    address2 = models.CharField(max_length=200, null=True, blank=True)
+    address3 = models.CharField(max_length=200, null=True, blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True)
+    pin = models.IntegerField(null=True, blank=True)
+    amc_aum_date = models.DateTimeField(null=True, blank=True)
+
+"""
